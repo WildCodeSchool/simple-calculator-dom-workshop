0855194eb0b23c6ca27eafc5139c92182d2b2165
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2020-09-10 17:50:13 +0200

    Updates

---
+++ precache-manifest.cecf8580ad4d7ceec33c06f84371bebd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a96a7ebd483e4825e85ae3721d02524",
+    "url": "/simple-calculator-dom-workshop/index.html"
+  },
+  {
+    "revision": "ae22ae00f01e55fb3fdf",
+    "url": "/simple-calculator-dom-workshop/static/js/2.bb51c59f.chunk.js"
+  },
+  {
+    "revision": "e88041e5e53e77714ead",
+    "url": "/simple-calculator-dom-workshop/static/js/main.702b89b9.chunk.js"
+  },
+  {
+    "revision": "0f4365c83868e84d46bb",
+    "url": "/simple-calculator-dom-workshop/static/js/runtime~main.7fab3c0a.js"
+  }
+]);
